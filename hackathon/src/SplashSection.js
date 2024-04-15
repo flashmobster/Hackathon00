61ff6f8ad4ec6e0ffa081d874567211ddb74f023
flashmobster <crishoyle@gmail.com>
2024-04-15 12:07:59 -0500

    updated to generated logo

--- hackathon/src/SplashSection.js
+++ hackathon/src/SplashSection.js
@@ -5,7 +5,7 @@ const SplashSection = () => {
     return (
         <div className="splash-section">
             <h1>Welcome to Digital Delta, Home of the Grizzlies Stats</h1>
-            <img className='logo' src="/Memphis Grizzlies Alternate Logo History.jpeg" alt="Grizzlies Slash Logo"></img>
+            <img className='logo' src="/HokWsgRC.jpeg" alt="Grizzlies Slash Logo"></img>
             <p>We've got your Grizzlies Info!</p>
         </div>
     );
