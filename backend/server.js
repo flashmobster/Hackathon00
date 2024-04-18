2a7cefc446317235d0a1c7b913b9e4fc85f88f2e
flashmobster <crishoyle@gmail.com>
2024-04-18 13:40:10 -0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -11,34 +11,16 @@ app.use(cors());
 app.use(express.json());
 
 const API_KEY = process.env.BDL_API_KEY;
-const TEAM_ID = 15; 
 
 // Route to get players from the BallDontLie API
 app.get('/', async (req, res) => {
     try {
-        let allPlayers = [];
-        let page = 1;
-        let totalPages = 1; // page one to begin
-
-        // Fetch all pages of players
-        while (page <= totalPages) {
-            const { data } = await axios.get(`https://api.balldontlie.io/v1/players`, {
-                params: {
-                    per_page: 100,
-                    team_ids: TEAM_ID,
-                    page: page
-                },
-                headers: {
-                    Authorization: `${API_KEY}`
-                }
-            });
-
-            allPlayers = [...allPlayers, ...data.data];
-            totalPages = data.meta.total_pages;
-            page++;
-        }
-
-        res.json(allPlayers);
+        const { data } = await axios.get("https://api.balldontlie.io/v1/players?per_page=100&team_ids[]=15", {
+            headers: {
+                Authorization: `${API_KEY}`
+            }
+        });
+        res.json(data);
     } catch (error) {
         console.error('Error fetching players:', error);
         res.status(500).json({ message: 'Failed to fetch players' });
