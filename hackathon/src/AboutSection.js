bb1d47577e2f979cf8ec54d740afb551fea93b16
flashmobster <crishoyle@gmail.com>
2024-04-19 04:14:30 -0500

    updated

--- hackathon/src/AboutSection.js
+++ hackathon/src/AboutSection.js
@@ -5,8 +5,8 @@ const AboutSection = () => {
     return (
         <div className="about-section">
             <h2>About NBA Stats</h2>
-            <p>This app will return stats for key retired players in Memphis Grizzlies history.</p>
-            <p>By doing this, our website streamlines information gathering on popular retired Grizzlies' name, position, and years with the team. No need to filter through sites that may be bloated.</p>
+            <p>This app will return stats for players in Memphis Grizzlies history.</p>
+            <p>By doing this, our website streamlines information gathering on Grizzlies' players profiles. No need to filter through sites that may be bloated.</p>
         </div>
     );
 };
