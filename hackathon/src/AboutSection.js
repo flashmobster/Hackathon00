f1f894babdda204e0c699de1d75a4f81a28a9de9
flashmobster <crishoyle@gmail.com>
2024-10-16 18:33:51 -0500

    added misc Poole image for fantasy basketball logo

--- hackathon/src/AboutSection.js
+++ hackathon/src/AboutSection.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './AboutSection.css'; 
+import poole from './src/Screenshot_20241016_144806_Chrome.jpg'
 
 const AboutSection = () => {
     return (
         <div className="about-section">
             <h2>About NBA Stats</h2>
+            <img>
+                src={poole}
+                alt="Poole & The Gang"
+                style{{width: '400px', height: '400px'}}
+            </img>
             <p>This app will return stats for players in Memphis Grizzlies history.</p>
             <p>By doing this, our website streamlines information gathering on Grizzlies' players profiles. No need to filter through sites that may be bloated.</p>
         </div>
+        
     );
 };
 
